a2c668d434c5cea8e38497f1e118fcddb6240823
BenMaRe-Dev <b.maguinareyes@gmail.com>
2020-05-20 18:50:58 -0500

    Show List of Companies

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 games_data = [ 
-  {:name=>"Rune Factory 4 Special", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Jul-25"},
-  {:name=>"Borderlands 3", :genre=>"Simulator", :price=>5999, :release_date=>"2019-Sep-13"},
-  {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25"},
-  {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20"},
-  {:name=>"Monster Hunter: World - Iceborne", :genre=>"Adventure", :price=>6999, :release_date=>"2019-Sep-06"},
-  {:name=>"Cyberpunk 2077", :genre=>"Role-playing (RPG)", :price=>4999, :release_date=>"2020-Sep-17"},
-  {:name=>"Pokémon Shield", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Nov-15"},
-  {:name=>"Children of Morta", :genre=>"Role-playing (RPG)", :price=>6999, :release_date=>"2019-Sep-03"} ]
+  {:name=>"Rune Factory 4 Special", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Jul-25", company_id: 1},
+  {:name=>"Borderlands 3", :genre=>"Simulator", :price=>5999, :release_date=>"2019-Sep-13", company_id: 2},
+  {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25", company_id: 1},
+  {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20", company_id: 2},
+  {:name=>"Monster Hunter: World - Iceborne", :genre=>"Adventure", :price=>6999, :release_date=>"2019-Sep-06", company_id: 1},
+  {:name=>"Cyberpunk 2077", :genre=>"Role-playing (RPG)", :price=>4999, :release_date=>"2020-Sep-17", company_id: 2},
+  {:name=>"Pokémon Shield", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Nov-15", company_id: 1},
+  {:name=>"Children of Morta", :genre=>"Role-playing (RPG)", :price=>6999, :release_date=>"2019-Sep-03", company_id: 2} ]
 
 games_data.each do |game| 
   game = Game.create!(game)
