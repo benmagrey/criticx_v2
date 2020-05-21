eff5a27d6441bdcbac6f3dc98611cd84cef00205
BenMaRe-Dev <b.maguinareyes@gmail.com>
2020-05-20 20:59:02 -0500

    Create a New Company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @companies = Company.all
     render json: @companies 
@@ -6,5 +8,19 @@ class CompaniesController < ApplicationController
   def show
     @companies = Company.find(params[:id])
     render json: @companies
+  end
+
+  def create
+    @companies = Company.new(companies_params)
+    if @companies.save
+      render json: @companies
+    else
+      render json: @companies.errors
+    end
+  end
+    
+  private
+  def companies_params
+    params.require(:company).permit( :name, :description, :start_date, :country)
   end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @games = Game.all
     render json: @games
@@ -6,5 +8,19 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
     render json: @game
+  end
+  
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      render json: @game
+    else
+      render json: @game.errors
+    end
+  end
+    
+  private
+  def game_params
+    params.require(:game).permit( :name, :genre, :price, :release_date, :company_id)  
   end
 end
